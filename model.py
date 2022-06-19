002043c8f4f1889aaeb12bc1a445c935e88d07a9
Kehinde Hussein, Fasunle <kfasunle@gmail.com>
2022-06-19 13:25:03 +0100

    fix: Create User method refactored

--- model.py
+++ model.py
@@ -1,5 +1,5 @@
 from pymongo import MongoClient
-
+from bson.json_util import dumps
 from config import MONGO_DATABASE_URI
 
 client = MongoClient(MONGO_DATABASE_URI)
@@ -9,13 +9,14 @@ database = client.sloovi
 class User:
     """User Model Created with PyMongo
     """
-    
+    _user = database.user
+     
     def __init__(self, name, email, password_hash) -> None:
         self.name = name
         self.email = email
         self.password = password_hash
     
-    def create_user(self):
+    def create(self):
         """Create a new User with name, email and password
         
         If success:
@@ -29,20 +30,24 @@ class User:
         user_data = {
             "name": self.name,
             "email": self.email,
-            "password": self.password
+            "password": self.password           # TODO: password must be hashed!
         }
         
-        # check if user already exist
         
-        user = database.user.find({"email": self.email})
+        user_cursor = database.user.find_one({"email": self.email})
+        user = dumps(user_cursor)
         
         # TODO: Raise an ExeptionError with proper message
-        if len(user) != 0:
+        
+        # check if user already exist
+        if user != 'null':
             return f"User with email {self.email} Already exist!", 400
         
-        try:
-            database.user.insert_one(user_data)
-        except :
-            return "An Error occured while creating a new User. Please try again!", 400
-        
-        return f"A new User with email {self.email} was created."
+        else:
+            # if the user does not exist already, create it
+            try:
+                self._user.insert_one(user_data)
+            except :
+                return "An Error occured while creating a new User. Please try again!", 400
+            
+            return f"A new User with email {self.email} was created."
