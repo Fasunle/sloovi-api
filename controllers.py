66c542d10add5e539e6d3856fe772b7e86b0179c
Kehinde H. Fasunle <kfasunle@gmail.com>
2022-07-16 02:01:19 +0100

    template controllers

--- controllers.py
+++ controllers.py
@@ -1,10 +1,11 @@
 
 
 
-from flask import Blueprint, redirect, request, url_for
-from model import create_user, fetch_user
+import json
+from flask import Blueprint, jsonify, redirect, request, url_for
+from model import create_new_template, create_user, delete_template, fetch_one_template, fetch_templates, fetch_user, update_template
 from sloovi_utils import generate_hash, generate_token
-from validators import validate_user_data, validate_user_login
+from validators import validate_template_data, validate_user_data, validate_user_login
 
 
 api_v1  = Blueprint("v1", __name__);
@@ -49,7 +50,7 @@ def register_user():
     info = create_user(name, email, password);
     status_code = info[1];
 
-    if status_code is not 200:
+    if status_code != 200:
         return info;
     
     else:
@@ -99,3 +100,106 @@ def login_user():
         
     # generate token
     return generate_token(user)
+
+
+@api_v1.route("/template", methods=["GET"])
+def show_templates_controller():
+    """Get All Template
+
+    Returns:
+        list: A list of all templates
+    """
+    
+    templates = fetch_templates()
+    return jsonify(templates)
+
+
+@api_v1.route("/template/<string:id>", methods=["GET"])
+def show_template_controller(id):
+    """Get A Template
+
+    Args:
+        id (int): template_id
+
+    Returns:
+        object: template
+    """
+    template = fetch_one_template(id)
+    return jsonify(template)
+
+
+@api_v1.route("/template", methods=["POST"])
+def create_template_controller():
+    """Create a new template
+    
+        {
+            'template_name': ' ',
+            'subject': ' ',
+            'body': ' ',
+        } 
+
+    Returns:
+        - a success message
+        - created a new template
+    """
+    # parse data from the client
+    name = request.json.get("template_name")
+    subject = request.json.get("subject")
+    body = request.json.get("body")
+
+    data = {
+        "template_name": name,
+        "subject": subject,
+        "body": body
+    }
+    
+    error = validate_template_data(data)
+    
+    if error is not None:
+        return error
+    
+    template = create_new_template(name, subject, body)
+    return template
+
+
+@api_v1.route("/template/<string:id>", methods=["PUT"])
+def update_template_controller(id):
+    """Update Single Template
+
+    Args:
+        id (int): template_id
+
+    Returns:
+        - success message
+    """
+    # parse data from the client
+    data = json.loads(request.data)
+    
+    error = validate_template_data(data)
+    
+    if error is not None:
+        return error
+    
+    name = data.get("template_name")
+    subject = data.get("subject")
+    body = data.get("body")
+    
+    template = update_template(id, name, subject, body)
+    return template
+
+
+@api_v1.route("/template/<string:id>", methods=["DELETE"])
+def delete_template_controller(id):
+    """Delete Single Template
+
+    Args:
+        id (int): template_id
+
+    Returns:
+        - success message
+    """
+    deleted_template = delete_template(id)
+    if deleted_template is None:
+        return f"Template with this ID: {id} does not exist!"
+    
+    return delete_template
